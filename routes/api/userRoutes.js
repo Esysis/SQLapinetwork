b6309f055b824014600bab7ffd80c0baabe06892
Yuga <Trev@brennberry.com>
2023-08-14 19:06:08 -0400

    remainder of userRoutes code added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,3 +13,16 @@ router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
+
+  router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+module.exports = router;
