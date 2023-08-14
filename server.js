f1bb1af5b263deeeb39ecbb715b8697b06e39c27
Yuga <Trev@brennberry.com>
2023-08-14 19:24:39 -0400

    Added the proper routes in the server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const userRoutes = require('./routes/api/userRoutes');
+const thoughtRoutes = require('./routes/api/thoughtRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
 mongoose.connect('mongodb://localhost/social_network', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
