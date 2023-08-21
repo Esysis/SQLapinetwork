cd18b445a7d36c3ddd195d342cf4c4aeb7358618
Yuga <Trev@brennberry.com>
2023-08-21 09:39:06 -0400

    Fixed stuff in package changed stuff in server

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-const userRoutes = require('./routes/api/userRoutes');
-const thoughtRoutes = require('./routes/api/thoughtRoutes');
-
-app.use('/api/users', userRoutes);
-app.use('/api/thoughts', thoughtRoutes);
-
-mongoose.connect('mongodb://localhost/social_network', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Api running on port ${PORT}!`);
+    });
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
